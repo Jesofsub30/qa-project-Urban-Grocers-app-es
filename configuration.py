7755f394efd1041c2aa343691cd3721f9f36fd3b
Jesofsub30 <biojerour@gmail.com>
2024-10-23 23:13:29 -0600

    Se hace referencia a la URL y endpoints necesarios para acceder a la API

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-f1f2b32b-52cc-4276-9a1d-b24bd669be9b.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-1c3cf356-cc1e-47be-861e-b57eab4db914.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH ="/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
