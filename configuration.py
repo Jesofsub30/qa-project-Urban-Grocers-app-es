94bcfbfb0c070afa89bdcaec9fa09b800eaf5f96
Jesofsub30 <biojerour@gmail.com>
2024-10-27 17:43:10 -0600

    Se hace referencia a la URL y los endpoints necesarios para llevar acabo las pruebas

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-1c3cf356-cc1e-47be-861e-b57eab4db914.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-4c1744f5-cff9-47f6-81e5-97055bff542a.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH ="/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
